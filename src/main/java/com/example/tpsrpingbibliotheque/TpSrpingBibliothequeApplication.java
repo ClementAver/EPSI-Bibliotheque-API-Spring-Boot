3c27caca4d1e76539b8a36ce09698be699b228d6
ClementAver <clement.aver@yahoo.fr>
2024-06-06 11:28:34 +0200

    init

---
+++ src/main/java/com/example/tpsrpingbibliotheque/TpSrpingBibliothequeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpsrpingbibliotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSrpingBibliothequeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpSrpingBibliothequeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tpsrpingbibliotheque/TpSrpingBibliothequeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpsrpingbibliotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSrpingBibliothequeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
